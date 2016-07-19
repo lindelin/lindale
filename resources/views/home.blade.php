928fd04725d35432314f2c63862891e46c52589b
Lindale <kudouyoichi@users.noreply.github.com>
2016-07-19 15:58:41 +0900

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -57,7 +57,7 @@
 			<nav class="navbar navbar-default navbar-fixed-bottom">
 			   <div class="navbar-inner navbar-content-center">
 			      <p class="text-muted credit" style="padding: 10px;">
-        			 ....
+        			 Power by Lindale
 			      </p>
 		           </div>
 		        </nav>
