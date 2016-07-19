f0eaa20c8d1a2bbead3e47be71ce3bdf4471ff71
Lindale <kudouyoichi@users.noreply.github.com>
2016-07-19 15:57:14 +0900

    add footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -53,6 +53,15 @@
 		        </ul>
 		    </div>
 		</div>
+		<div id="footer" class="container">
+			<nav class="navbar navbar-default navbar-fixed-bottom">
+			   <div class="navbar-inner navbar-content-center">
+			      <p class="text-muted credit" style="padding: 10px;">
+        			 ....
+			      </p>
+		           </div>
+		        </nav>
+		</div>       
 	</div>
 </div>	
 </body>
