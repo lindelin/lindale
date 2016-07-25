025e3b695521f639b480dfc72e0a521d64df3da8
Lindale <kudouyoichi@users.noreply.github.com>
2016-07-25 18:30:00 +0900

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,7 +8,7 @@
     <title>Lindalë-Admin</title>
 
     <link href="//cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
     <link href="{{ asset('/css/main.css') }}" rel="stylesheet" type="text/css">
     <script src="//cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
     <script src="//cdn.bootcss.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
