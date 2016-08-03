4b6e4784d6d5894719061addb93af032d24a3981
kgo <kgo@fourmix.co.jp>
2016-08-03 13:17:51 +0900

    验证评论表单

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -13,6 +13,13 @@ class CommentController extends Controller
 {
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'nickname' => 'required|max:40',
+            'email' => 'max:100',
+            'website' => 'max:100',
+            'content' => 'required|max:255',
+        ]);
+
         if (!Article::find($request->article_id)) {
             return redirect()->to("/")->withInput()->withErrors('评论发表失败！');
         } else if (Comment::create($request->all())) {
