1dc782315780ab78caa824fdbd4c1078cdb5c1eb
kgo <kgo@fourmix.co.jp>
2016-08-04 23:47:22 +0900

    add test case

---
+++ tests/FrontRoutesTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class FrontRoutesTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Test the response code for the Top page.
+     *
+     * @return void
+     */
+    public function testTop()
+    {
+        $response = $this->call('GET', '/');
+
+        $this->assertEquals(200, $response->status());
+    }
+
+    /**
+     * Test the response code for the Posts page.
+     *
+     * @return void
+     */
+    public function testAritcle()
+    {
+        $response = $this->call('GET', '/article/1');
+
+        $this->assertEquals(200, $response->status());
+    }
+
+}
