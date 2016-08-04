6d60b2837246b2dc74945435bcf2a711cb4e7ffa
kgo <kgo@fourmix.co.jp>
2016-08-04 23:41:52 +0900

    CI

--- app/Article.php
+++ app/Article.php
@@ -4,6 +4,25 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Article
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Comment[] $hasManyComments
+ * @property-read \App\User $hasOneUser
+ * @mixin \Eloquent
+ * @property integer $id
+ * @property string $title
+ * @property string $body
+ * @property integer $user_id
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereTitle($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereBody($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereUserId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereUpdatedAt($value)
+ */
 class Article extends Model
 {
     public function hasManyComments()
--- app/Comment.php
+++ app/Comment.php
@@ -4,6 +4,28 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Comment
+ *
+ * @property-read \App\Article $hasOneArticle
+ * @mixin \Eloquent
+ * @property integer $id
+ * @property string $nickname
+ * @property string $email
+ * @property string $website
+ * @property string $content
+ * @property integer $article_id
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Comment whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Comment whereNickname($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Comment whereEmail($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Comment whereWebsite($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Comment whereContent($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Comment whereArticleId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Comment whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Comment whereUpdatedAt($value)
+ */
 class Comment extends Model
 {
     protected $fillable = ['nickname', 'email', 'website', 'content', 'article_id'];
