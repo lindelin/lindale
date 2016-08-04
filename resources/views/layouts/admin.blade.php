d3b29bb397ab8b8d8388af8a3462f28ba6dc2807
kgo <kgo@fourmix.co.jp>
2016-08-05 01:14:04 +0900

    调整

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,12 +7,8 @@
 
     <title>Lindalë-Admin</title>
 
-    <link href="//cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-    <link href="{{ asset('/css/main.css') }}" rel="stylesheet" type="text/css">
-    <script src="//cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
-    <script src="//cdn.bootcss.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    <script src="//cdn.ckeditor.com/4.5.10/standard/ckeditor.js"></script>
+    @include('layouts.link')
+
 </head>
 <body>
 
