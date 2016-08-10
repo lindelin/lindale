7a679f20c7c98c54926c1320aed28afff1f8a8f0
kgo <kgo@fourmix.co.jp>
2016-08-10 19:08:59 +0900

    データベース初期設定変更

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        \App\User::create([
+            'name'   => 'Admin',
+            'email'    => 'admin@lindale.tk',
+            'password' => bcrypt("lindale.tk"),
+        ]);
+    }
+}
