e0f5eb84725fb491221bc796662c52827694d7f7
Lindale <kudouyoichi@gmail.com>
2016-08-12 01:44:29 +0900

    DEBUG

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+    	$this->call(UserSeeder::class);
         $this->call(ArticleSeeder::class);
-        $this->call(UserSeeder::class);
     }
 }
