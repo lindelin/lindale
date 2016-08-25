628a6b9dcb31dee9b2d461790d93f63f2edfb5a0
Lindale <kudouyoichi@gmail.com>
2016-08-25 17:41:19 +0900

    修正

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
-        $this->call(ArticleSeeder::class);
+//         $this->call(UserSeeder::class);
+//         $this->call(ArticleSeeder::class);
         $this->call(TaskTypeSeeder::class);
         $this->call(TaskStatusSeeder::class);
         $this->call(TaskSeeder::class);
