892178d14cefc921c56cfbe562c3d590bd4f830f
Lindale <kudouyoichi@gmail.com>
2016-09-25 18:32:50 +0900

    mail:config

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'info-center@lindale.tk',
+        'name' => 'Lindale',
     ],
 
     /*
