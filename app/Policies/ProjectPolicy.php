25f29c8a5cb4dd6fffa0870ec2dd392922da19cb
kgo <kgo@fourmix.co.jp>
2016-10-11 08:41:27 +0900

    権限修正

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Definer;
 use App\Project\Project;
 use App\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
@@ -11,11 +12,27 @@ class ProjectPolicy
 {
     use HandlesAuthorization;
 
+    /**
+     * プロジェクトリーダーのみ（パスワード必要）
+     *
+     * @param User $user
+     * @param Project $project
+     * @param $request
+     * @return bool
+     */
     public function delete(User $user, Project $project, $request)
     {
         return $user->id === $project->user_id and Hash::check($request->get('password'), $project->password);
     }
 
+    /**
+     * プロジェクトリーダーとサブリーダーのみ（パスワード必要）
+     *
+     * @param User $user
+     * @param Project $project
+     * @param $request
+     * @return bool
+     */
     public function update(User $user, Project $project, $request)
     {
         if ($user->id === $project->user_id and Hash::check($request->get('project-pass'), $project->password)) {
@@ -27,14 +44,38 @@ class ProjectPolicy
         }
     }
 
+    /**
+     * プロジェクトリーダー
+     * サブリーダー
+     * マネージャー
+     *
+     * @param User $user
+     * @param Project $project
+     * @return bool
+     */
     public function member(User $user, Project $project)
     {
         if ($user->id === $project->user_id) {
             return true;
         } elseif ($user->id === $project->sl_id) {
             return true;
+        } elseif ($project->Users()->find($user->id)->pivot->is_admin === Definer::projectAdmin()) {
+            return true;
         } else {
             return false;
+        }
+    }
+
+    /**
+     * Super Admin
+     *
+     * @param User $user
+     * @return bool
+     */
+    public function before(User $user)
+    {
+        if (Admin::is_super_admin($user)) {
+            return true;
         }
     }
 }
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -10,6 +10,12 @@ class UserPolicy
 {
     use HandlesAuthorization;
 
+    /**
+     * スーパー管理人のみ
+     *
+     * @param User $user
+     * @return mixed
+     */
     public function admin(User $user)
     {
         return Admin::is_super_admin($user);
