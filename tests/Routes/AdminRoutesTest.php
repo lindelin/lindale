fdae805b924af48a738903cfdbaaecd52521e713
kgo <kgo@fourmix.co.jp>
2016-10-28 15:47:08 +0900

    新しいテストケース追加

---
+++ tests/Routes/AdminRoutesTest.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AdminRoutesTest extends TestCase
+{
+    /**
+     * ユーザモデル.
+     *
+     * @var App\User
+     */
+    private $user;
+    /**
+     * テストユーザ作成.
+     *
+     * @before
+     * @return void
+     */
+    public function createUser()
+    {
+        $this->user = \App\User::find(1);
+    }
+
+    /**
+     * プライベートルートとしてアクセスできる
+     *
+     * @test
+     */
+    public function it_can_access_the_admin_page()
+    {
+        $response = $this->actingAs($this->user)->call('GET', '/admin');
+        $this->assertEquals(200, $response->status());
+        $this->assertViewHasAll(['mode']);
+    }
+
+    /**
+     * プライベートルートとしてアクセスできる
+     *
+     * @test
+     */
+    public function it_can_access_the_admin_user_page()
+    {
+        $response = $this->actingAs($this->user)->call('GET', '/admin/user');
+        $this->assertEquals(200, $response->status());
+        $this->assertViewHasAll(['mode', 'users']);
+    }
+
+    /**
+     * プライベートルートとしてアクセスできる
+     *
+     * @test
+     */
+    public function it_can_access_the_admin_user_create_page()
+    {
+        $response = $this->actingAs($this->user)->call('GET', '/admin/user/create');
+        $this->assertEquals(200, $response->status());
+        $this->assertViewHasAll(['mode']);
+    }
+}
