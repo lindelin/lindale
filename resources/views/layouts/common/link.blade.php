d87516bed09f5729f9c6ae3411e73239b0db7019
kgo <kgo@fourmix.co.jp>
2016-12-28 16:59:21 +0900

    添加网站图标

--- resources/views/layouts/common/link.blade.php
+++ resources/views/layouts/common/link.blade.php
@@ -33,9 +33,26 @@
 <link rel="icon" href="{{ asset('/favicon.ico') }}" type="image/x-icon" />
 <link rel="bookmark" href="{{ asset('/favicon.ico') }}" type="image/x-icon" />
 <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}" type="image/x-icon">
-<link href="{{ asset('/apple-touch-icon.png') }}" rel="apple-touch-icon-precomposed">
-<link rel="icon" type="image/png" href="{{ asset('/apple-touch-icon.png') }}">
-<link rel="mask-icon" href="{{ asset('/website_icon.svg') }}" color="#FFFFFF">
+<link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ asset('/apple-touch-icon.png') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('/apple-touch-icon.png') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('/apple-touch-icon.png') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('/apple-touch-icon.png') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="60x60" href="{{ asset('/apple-touch-icon.png') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="120x120" href="{{ asset('/apple-touch-icon.png') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="76x76" href="{{ asset('/apple-touch-icon.png') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="152x152" href="{{ asset('/apple-touch-icon.png') }}" />
+<link rel="icon" type="image/png" href="{{ asset('/apple-touch-icon.png') }}" sizes="196x196" />
+<link rel="icon" type="image/png" href="{{ asset('/apple-touch-icon.png') }}" sizes="96x96" />
+<link rel="icon" type="image/png" href="{{ asset('/apple-touch-icon.png') }}" sizes="32x32" />
+<link rel="icon" type="image/png" href="{{ asset('/apple-touch-icon.png') }}" sizes="16x16" />
+<link rel="icon" type="image/png" href="{{ asset('/apple-touch-icon.png') }}" sizes="128x128" />
+<meta name="application-name" content="&nbsp;"/>
+<meta name="msapplication-TileColor" content="#FFFFFF" />
+<meta name="msapplication-TileImage" content="mstile-144x144.png" />
+<meta name="msapplication-square70x70logo" content="mstile-70x70.png" />
+<meta name="msapplication-square150x150logo" content="mstile-150x150.png" />
+<meta name="msapplication-wide310x150logo" content="mstile-310x150.png" />
+<meta name="msapplication-square310x310logo" content="mstile-310x310.png" />
 {{-- local js --}}
 
 <!-- Scripts -->
