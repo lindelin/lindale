9f0c0f13f831d865e4b0356499061b6449163f7f
kgo <kgo@fourmix.co.jp>
2017-03-06 09:44:59 +0900

    添加系统升级命令

---
+++ app/Console/Commands/Lindale/LindaleUpdate.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands\Lindale;
+
+use Illuminate\Console\Command;
+
+class LindaleUpdate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'Lindale:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update the Lindale Web application when pulled from git';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->comment(PHP_EOL.'Lindale Project Management System');
+
+        if ($this->confirm('Do you want to update the system?')) {
+
+            try{
+
+                $this->comment(PHP_EOL.'<info>Step 1/3: Caching configs...</info>');
+                Artisan::call('config:cache');
+
+                $this->comment(PHP_EOL.'<info>Step 2/3: Caching routes...</info>');
+                Artisan::call('route:cache');
+
+                $this->comment(PHP_EOL.'<info>Step 3/3: Optimizing...</info>');
+                Artisan::call('optimize');
+
+                $this->comment(PHP_EOL.'Successfully updated. <info>✔</info>');
+
+
+            }catch (\Exception $e){
+                $this->line($e);
+                $this->line(PHP_EOL.'<error>✘</error> System error. Update failed!');
+                exit;
+            }
+
+        }else{
+            $this->comment(PHP_EOL.'Thanks.');
+            exit;
+        }
+    }
+}
