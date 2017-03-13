590bbbc766a5fae1bef65507096b4c01c496244b
kgo <kgo@fourmix.co.jp>
2017-03-13 22:13:19 +0900

    進捗多言語対応

---
+++ resources/lang/en/progress.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | progress Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'schema-info'   => 'Schema',
+    'status'        => 'Activity',
+    'new-task'      => 'New Tasks',
+    'finished-task' => 'Completed Tasks',
+    'new-todo'      => 'New TODO',
+    'finished-todo' => 'Completed TODO',
+    'count'         => 'Unit',
+];
