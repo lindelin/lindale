9231893670af6c7416eafa73f63cb67288ab0a4f
kgo <kgo@fourmix.co.jp>
2017-03-15 23:08:54 +0900

    デザイン修正

--- resources/views/layouts/common/user-img.blade.php
+++ resources/views/layouts/common/user-img.blade.php
@@ -1,9 +1,9 @@
 @if($user_img->photo != '')
     <a href="#" class="thumbnail">
-        <img src="{{ asset('storage/'.$user_img->photo) }}" class="img-circle">
+        <img src="{{ asset('storage/'.$user_img->photo) }}" width="600" height="600">
     </a>
 @else
     <a href="#" class="thumbnail">
-        <img src="{{ asset(Colorable::lindaleProfileImg($user_img->email)) }}" class="img-circle">
+        <img src="{{ asset(Colorable::lindaleProfileImg($user_img->email)) }}" width="600" height="600">
     </a>
 @endif
