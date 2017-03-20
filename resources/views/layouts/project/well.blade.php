bf4d0a0f66840ce9c6961c9b2560e1384050ffdc
Lindelea <kgo@lindelin.org>
2017-03-20 22:13:00 +0900

    BUG修正

--- resources/views/layouts/project/well.blade.php
+++ resources/views/layouts/project/well.blade.php
@@ -1,4 +1,4 @@
-<div class="well well-home" style="height: 374px;">
+<div class="well well-home">
     <div class="row">
         <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
             <h4 class="lindale-icon-color">
