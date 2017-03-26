22f8d9625c2d0f238a4c7decead6112609af2165
Lindelea <kgo@lindelin.org>
2017-03-26 20:10:18 +0900

    AWS ELB対応

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
         PrettyRoutes\ServiceProvider::class,
+        Lindelea\Support\Aws\Elb\ServiceProvider::class,
 
         /*
          * Application Service Providers...
