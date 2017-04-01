45a0052a4ccd90aafa943daa779a54e82d9436ae
Lindelea <kgo@lindelin.org>
2017-04-01 17:12:21 +0000

    Apply fixes from StyleCI

--- app/Definer.php
+++ app/Definer.php
@@ -102,15 +102,15 @@ class Definer
     {
         if ($mode == 'basic') {
             return new HtmlString('<span class="glyphicon glyphicon-briefcase lindale-icon-color"></span> '.trans('config.basic'));
-        }elseif ($mode == 'locale') {
+        } elseif ($mode == 'locale') {
             return new HtmlString('<span class="glyphicon glyphicon-globe lindale-icon-color"></span> '.trans('config.locale'));
-        }elseif ($mode == 'notification') {
+        } elseif ($mode == 'notification') {
             return new HtmlString('<span class="glyphicon glyphicon-bell lindale-icon-color"></span> '.trans('config.notification'));
-        }elseif ($mode == 'taskType') {
+        } elseif ($mode == 'taskType') {
             return new HtmlString('<span class="glyphicon glyphicon-tag lindale-icon-color"></span> '.trans('config.task-type-config'));
-        }elseif ($mode == 'taskStatus') {
+        } elseif ($mode == 'taskStatus') {
             return new HtmlString('<span class="glyphicon glyphicon-dashboard lindale-icon-color"></span> '.trans('config.task-status-config'));
-        }else{
+        } else {
             return 'MENU';
         }
     }
