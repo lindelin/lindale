ff05d56d3cd0ebb9c70568fb87d7caf964eeecc0
lindelea <kgo@lindelin.org>
2017-04-18 10:39:02 +0900

    App icon 修正

--- resources/views/layouts/common/link.blade.php
+++ resources/views/layouts/common/link.blade.php
@@ -37,8 +37,14 @@
 <link rel="icon" type="image/png" href="{{ asset('/favicon-32x32.png') }}" sizes="32x32">
 <link rel="icon" type="image/png" href="{{ asset('/favicon-16x16.png') }}" sizes="16x16">
 <link rel="manifest" href="{{ asset('/manifest.json') }}">
-<link rel="mask-icon" href="{{ asset('/safari-pinned-tab.svg') }}" color="#2760cf">
+<link rel="mask-icon" href="{{ asset('/safari-pinned-tab.svg') }}" color="#00184a">
+<meta name="apple-mobile-web-app-title" content="Lindal&euml;">
+<meta name="application-name" content="Lindal&euml;">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="{{ asset('/mstile-144x144.png') }}">
 <meta name="theme-color" content="#ffffff">
+
+
 {{-- facebook meta --}}
 
 
