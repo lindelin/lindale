f76c09da29109aa38e829b5bb6c48534992f2c7b
Lindelea <kgo@lindelin.org>
2017-06-13 00:20:23 +0900

    laravel 5.4 テストケース追加

--- routes/web.php
+++ routes/web.php
@@ -286,7 +286,7 @@ Route::group(['middleware' => ['auth', 'AdminAuth'], 'namespace' => 'Admin', 'pr
 |
 */
 
-Route::get('login', 'Auth\LoginController@showLoginForm');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout');
 
