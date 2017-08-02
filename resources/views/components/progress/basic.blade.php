12486eef499fee28acf8a3fff25ff331bfde367b
lindelea <kgo@lindelin.org>
2017-08-02 10:58:30 +0900

    スマホ日本語版進捗概要ラベル改行修正

--- resources/views/components/progress/basic.blade.php
+++ resources/views/components/progress/basic.blade.php
@@ -1,10 +1,10 @@
 <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <div class="row">
-            <div class="col-xs-3 col-sm-3 col-md-4 col-lg-3">
+            <div class="col-xs-4 col-sm-3 col-md-4 col-lg-3">
                 <span class="glyphicon glyphicon-dashboard lindale-icon-color"></span> {{ trans('header.progress') }}
             </div>
-            <div class="col-xs-9 col-sm-9 col-md-8 col-lg-9">
+            <div class="col-xs-8 col-sm-9 col-md-8 col-lg-9">
                 <div class="progress">
                     <div class="progress-bar progress-bar-success progress-bar-striped active" style="width: {{ $progress }}%">
                         {{ $progress }}% Complete
@@ -14,10 +14,10 @@
         </div>
 
         <div class="row">
-            <div class="col-xs-3 col-sm-3 col-md-4 col-lg-3">
+            <div class="col-xs-4 col-sm-3 col-md-4 col-lg-3">
                 <span class="glyphicon glyphicon-tasks lindale-icon-color"></span> {{ trans('header.tasks') }}
             </div>
-            <div class="col-xs-9 col-sm-9 col-md-8 col-lg-9">
+            <div class="col-xs-8 col-sm-9 col-md-8 col-lg-9">
                 <div class="progress">
                     <div class="progress-bar progress-bar-warning progress-bar-striped active" style="width: {{ $task_progress }}%">
                         {{ $task_progress }}% Complete
@@ -27,10 +27,10 @@
         </div>
 
         <div class="row">
-            <div class="col-xs-3 col-sm-3 col-md-4 col-lg-3">
+            <div class="col-xs-4 col-sm-3 col-md-4 col-lg-3">
                 <span class="glyphicon glyphicon-check lindale-icon-color"></span> TODO
             </div>
-            <div class="col-xs-9 col-sm-9 col-md-8 col-lg-9">
+            <div class="col-xs-8 col-sm-9 col-md-8 col-lg-9">
                 <div class="progress" style="margin-bottom: 0px;">
                     <div class="progress-bar progress-bar-striped active"  style="width: {{ $todo_progress }}%">
                         {{ $todo_progress }}% Complete
