205d46315e0f7b5509c8c23cdd71b999d9de7ab4
lindelea <kgo@lindelin.org>
2017-08-28 12:36:01 +0900

    sh修正

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@
 sudo chown -R www-data:www-data .
 sudo chmod -R 777 storage
 sudo chmod -R 777 bootstrap/cache
-sudo chmod 600 storage/oauth*
+sudo chmod 600 storage/oauth-private.key
+sudo chmod 600 storage/oauth-public.key
 sudo php artisan queue:restart
 sudo supervisorctl restart all
