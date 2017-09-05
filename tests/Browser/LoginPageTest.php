2a7f623fa2464dad4f6a3994d3e8c5ffa139a2d3
Lindelea <kgo@lindelin.org>
2017-09-05 23:57:03 +0900

    ログイン画面シナリオテスト

---
+++ tests/Browser/LoginPageTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\Browser\Pages\LoginPage;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class LoginPageTest extends DuskTestCase
+{
+    /**
+     * Test Login Page
+     *
+     * @return void
+     */
+    public function testLoginPage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new LoginPage())
+                ->assertSee('E-Mail Address')
+                ->assertSee('Remember Me')
+                ->assertSee('Forgot Your Password?')
+                ->assertSee('English')
+                ->assertSee('Login')
+                ->lang();
+            ;
+        });
+    }
+
+    /**
+     * Test Login Page Reset Password Link
+     */
+    public function testLoginPageResetPasswordLink()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new LoginPage())
+                ->clickLink('忘记密码?')
+                ->assertPathIs('/password/reset')
+            ;
+        });
+    }
+
+    /**
+     * ログインテスト
+     */
+    public function testLoginFunction()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new LoginPage())
+                ->type('email', 'admin@lindelin.org')
+                ->type('password', '123456')
+                ->pause(2000)
+                ->click('#login-button')
+                ->assertPathIs('/home')
+            ;
+        });
+    }
+}
