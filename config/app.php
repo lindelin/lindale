a8b2003b62f272e9e20c9458011285e98c91ca7a
Lindelea <kgo@lindelin.org>
2017-09-11 19:32:18 +0900

    修正

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'Lindalë'),
     'title' => 'Lindalë - The Project Manager For Everyone.',
-    'version' => 'v1.5.2',
+    'version' => 'v1.5.4',
 
     /*
     |--------------------------------------------------------------------------
@@ -196,6 +196,7 @@ return [
         App\Providers\RoutingManagementServiceProvider::class,
         App\Providers\ToolServiceProvider::class,
         App\Providers\ConfigSystemServiceProvider::class,
+        App\Providers\RepositoryServiceProvider::class,
 
     ],
 
