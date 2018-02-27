63e859352b9b38e89f6a6d364d2cc87c8e67cc5f
lindelea <kgo@lindelin.org>
2018-02-27 17:55:39 +0900

    センター移設修正

--- app/Http/Middleware/UseSSL.php
+++ app/Http/Middleware/UseSSL.php
@@ -16,7 +16,7 @@ class UseSSL
      */
     public function handle($request, Closure $next)
     {
-        if (! $request->secure()) {
+        if (! $request->secure() and ! App::environment('testing')) {
             return redirect()->secure($request->getRequestUri());
         }
 
