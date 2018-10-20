9af154dc1813f9ec198b3d4d2a6e844b8129d406
Lindelea <kgo@lindelin.org>
2018-10-20 15:43:55 +0900

    設定API

--- app/Http/Controllers/Api/SettingsController.php
+++ app/Http/Controllers/Api/SettingsController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\System\Contracts\ConfigSystem\UserConfigSystemContract;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class SettingsController extends Controller
 {
+    /**
+     * 用户配置系统
+     *
+     * @var UserConfigSystemContract
+     */
+    protected $configSystem;
+
+    /**
+     * 构造器.
+     *
+     * LocaleController constructor.
+     * @param UserConfigSystemContract $configSystem
+     */
+    public function __construct(UserConfigSystemContract $configSystem)
+    {
+        $this->configSystem = $configSystem;
+    }
+
     /**
      * 言語&地区　設定
      * @return \Illuminate\Http\JsonResponse
@@ -19,5 +38,24 @@ class SettingsController extends Controller
             'current_language_name' => config('app.available_language.' . $lang),
             'options' => config('app.available_language'),
         ], 200);
+    }
+
+    /**
+     * 言語&地区　更新
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function updateLocale(Request $request)
+    {
+        $this->validate($request, [
+            'language' => 'required'
+        ]);
+
+        $this->configSystem->set($request->user(), config('config.user.lang'), $request->input('language'));
+
+        //$locale = user_config($request->user(), config('config.user.lang'));
+        //App::setLocale($locale);
+
+        return response()->json(['status' => 'OK'], 200);
     }
 }
