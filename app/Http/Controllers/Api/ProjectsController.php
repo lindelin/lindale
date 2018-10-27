ebccf51412e9db62204656f8a5e4034948496859
Lindelea <kgo@lindelin.org>
2018-10-27 16:00:33 +0900

    お気に入りプロジェクトAPI

--- app/Http/Controllers/Api/ProjectsController.php
+++ app/Http/Controllers/Api/ProjectsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Resources\ProjectCollection;
 use App\Project\Project;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
 {
@@ -15,5 +16,15 @@ class ProjectsController extends Controller
     public function resources()
     {
         return new ProjectCollection(Project::orderBy('progress', 'asc')->latest()->Paginate(30));
+    }
+
+    /**
+     * お気に入りプロジェクト
+     * @param Request $request
+     * @return ProjectCollection
+     */
+    public function favorites(Request $request)
+    {
+        return new ProjectCollection($request->user()->favorites);
     }
 }
