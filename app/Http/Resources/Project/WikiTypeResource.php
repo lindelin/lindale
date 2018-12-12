0d38d709872166c1ca013a24458ff65a0230a80d
lindelea <kgo@lindelin.org>
2018-12-12 12:52:51 +0900

    project wiki api

--- app/Http/Resources/Project/WikiTypeResource.php
+++ app/Http/Resources/Project/WikiTypeResource.php
@@ -14,6 +14,9 @@ class WikiTypeResource extends Resource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => trans($this->name),
+        ];
     }
 }
