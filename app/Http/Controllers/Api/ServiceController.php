f5f304f9b756024c8c4276a10f97b0644151c280
lindelea <kgo@lindelin.org>
2018-12-17 11:28:01 +0900

    Apple App Site Association

---
+++ app/Http/Controllers/Api/ServiceController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ServiceController extends Controller
+{
+    /**
+     * Apple App Site Association
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function appleAppSiteAssociation()
+    {
+        return response()->json([
+            'webcredentials' => [
+                'apps' => [
+                    '58MDHAE78Q.org.lindelin.lindale-ios',
+                ]
+            ]
+        ], 200);
+    }
+}
