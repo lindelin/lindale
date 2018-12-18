9ed0e93c4734141809d0027e68af8be0ed6e6f46
lindelea <kgo@lindelin.org>
2018-12-18 13:24:47 +0900

    Device Token 保存 API

--- database/migrations/2018_12_17_181018_create_devices_table.php
+++ database/migrations/2018_12_17_181018_create_devices_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDeviceTokensTable extends Migration
+class CreateDevicesTable extends Migration
 {
     /**
      * Run the migrations.
