53a13d00f9710babf479ff0831083f47deb3c31e
Lindelea <kgo@lindelin.org>
2018-12-19 21:43:30 +0900

    FCM API

--- app/Services/ApplePushNotificationService.php
+++ app/Services/ApplePushNotificationService.php
@@ -125,6 +125,7 @@ class ApplePushNotificationService
                         'subtitle' => $this->subtitle,
                         'sound' => 'default',
                         'click_action' => $this->category,
+                        'task_id' => 1,
                     ]
                 ],
             ]);
