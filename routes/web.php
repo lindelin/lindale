974aae6668128d3ccf1871ff417915f678ff4c77
lindelea <kgo@lindelin.org>
2018-12-26 10:50:52 +0900

    PUSH 通知

--- routes/web.php
+++ routes/web.php
@@ -344,8 +344,3 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 | 测试路由
 |
 */
-
-//Route::get('test', function () {
-//    $users = \App\User::with(['devices'])->whereHas('devices')->get();
-//    dd(\App\Models\User\Device::tokens($users));
-//});
