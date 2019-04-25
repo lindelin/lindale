676d0310d527fafc5fa3c27fdda337f7a41dc18e
lindelea <kgo@lindelin.org>
2019-04-25 18:33:09 +0900

    home Dashboard 部品

---
+++ resources/assets/js/vue/basic/charts/UserActivityChart.vue
@@ -0,0 +1,10 @@
+<script>
+    import { Area } from 'vue-chartjs'
+    export default {
+        extends: Area,
+        props: ['chartdata', 'options'],
+        mounted () {
+            this.renderChart(this.chartdata, this.options)
+        }
+    }
+</script>
