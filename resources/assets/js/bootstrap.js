27edc7538d312f1b0cfd5c0d7649a7e9f1adfaa7
lindelea <kgo@lindelin.org>
2019-05-08 16:17:14 +0900

    IE 対応

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -20,6 +20,8 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
+require('es6-promise').polyfill();
+
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
