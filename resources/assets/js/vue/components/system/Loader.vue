d48ceb7e21fa7b5ec42d43079c8029d39801df32
lindelea <kgo@lindelin.org>
2019-05-10 15:37:23 +0900

    loader

---
+++ resources/assets/js/vue/components/system/Loader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="loader" v-if="">
+        <img src="/images/loader.svg">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Loader",
+        data: function () {
+            return {
+                show: true
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
